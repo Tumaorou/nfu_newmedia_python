8c1f6d82510e5cf72490ebf1b9ec42f113e13f8b
KOUJII <981004678@QQ.COM>
2017-06-16 20:14:24 +0800

    上传输出页面的代码及修改其他代码

--- game_hero_data/input_data.py
+++ game_hero_data/input_data.py
@@ -9,18 +9,17 @@ from pprint import pprint
 def get_dict_ename():
     with open('data/champion.json','r') as file:
         a = json.load(file)
-    dict_id = {x['ename']:{'cname':x['cname'],'title':x['title']} for x in a['data']}
-    return(dict_id)
-
-
+    dict_ename = {x['ename']:{'cname':x['cname'],'title':x['title']} for x in a['data']}
+    return(dict_ename)
+    
 def change_name(a):
-    dict_id = get_dict_ename()
-    for ename in dict_id:
-        if (a in dict_id[ename]['title']) or (a in dict_id[ename]['cname']):
+    dict_ename = get_dict_ename()
+    for ename in dict_ename:
+        if (a in dict_ename[ename]['title']) or (a in dict_ename[ename]['cname']):
             return(ename)
 
 def get_dict_hero(name):
-    dict_id = get_dict_ename()
+    dict_ename = get_dict_ename()
     try:
         ename = change_name(name)
         with open('data/{ename}.json'.format(ename=ename),'r') as fp:
