df17c09939439283d32811b55c4ebdd37a897c4f
KOUJII <981004678@QQ.COM>
2017-06-19 11:38:39 +0800

    添加视频，更新搜索框

--- game_hero_data/game_hero_data.py
+++ game_hero_data/game_hero_data.py
@@ -1,8 +1,12 @@
 from input_data import get_dict_ename,change_name,get_dict_hero
 from flask import Flask, render_template, request, escape
 
-
-
+dict_ename = get_dict_ename()
+list_name = list()
+for ename in dict_ename:
+    list_name.append(dict_ename[ename]['cname'])
+    list_name.append(dict_ename[ename]['title'])
+    
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,6 +16,7 @@ def entry_page() -> 'html':
     output_prompt = '请输入你想要搜索的英雄名'
     return render_template('entry.html',
                            the_output_prompt=output_prompt,
+                           the_list_name = list_name,
                            the_entry_title='英雄联盟英雄技能信息')
 
 
@@ -31,6 +36,7 @@ def do_search() -> 'html':
         return render_template('results.html',
                                the_title=dict_ename[hero_ename]['title'],
                                the_cname=dict_ename[hero_ename]['cname'],
+                               output_vidio = dict_hero['data'][0]['skins'][0]['source'],
                                output_skill_passive_name = dict_hero['data'][0]['passive']['name'],
                                output_skill_passive_description = dict_hero['data'][0]['passive']['description'],
                                output_skill_1_name = dict_hero['data'][0]['spells'][0]['name'],
